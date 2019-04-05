26cb5a53e0f8e0fbe5e8860b81a66dbb2015b34f
arielbeje <arielbeje@protonmail.com>
2019-04-05 22:32:53 +0300

    PEP8

--- tweet.py
+++ tweet.py
@@ -24,6 +24,7 @@ def setup_twitter():
     auth.set_access_token(credentials["twitter"]["access_token"], credentials["twitter"]["access_token_secret"])
     return tweepy.API(auth)
 
+
 def setup_reddit():
     """
     Set up the subreddit object and return the object
@@ -36,6 +37,7 @@ def setup_reddit():
     ani_bm_subreddit = reddit.subreddit("ani_bm")
     return ani_bm_subreddit
 
+
 def setup_bitly():
     """
     Set up the bit.ly object and return the object
@@ -43,33 +45,36 @@ def setup_bitly():
     bitly = bitly_api.Connection(access_token=credentials["bitly"]["api_key"])
     return bitly
 
+
 def choose_post(ani_bm_subreddit):
     """
     Choose the post object randomally (while preventing double-posting) and return the object
     """
     # Choose the post out of the 20 hottest posts on the subreddit
-    hot = ani_bm_subreddit.hot(limit = 20)
+    hot = ani_bm_subreddit.hot(limit=20)
     post = random.choice(list(hot))
 
     # Open the double-post prevent file and check whether or not the post has been already posted
     while post.id in open("already_tweeted.txt").read():
         print("Double-post, choosing a different post")
-        hot = ani_bm_subreddit.hot(limit = 20)
+        hot = ani_bm_subreddit.hot(limit=20)
         post = random.choice(list(hot))
 
     # Write the chosen post to the double-post prevent file
     already_tweeted = open("already_tweeted.txt", "a")
     already_tweeted.write(f"{post}\n")
-    
+
     # Close the file and return the post
     already_tweeted.close()
     return post
+
 
 def shorten_link(long_url, bitly):
     """
     Gets the URL to shorten and return the shortened URL
     """
-    return bitly.shorten(uri = f"https://reddit.com{long_url}"[:49])["url"]
+    return bitly.shorten(uri=f"https://reddit.com{long_url}"[:49])["url"]
+
 
 def setup_current_time():
     """
@@ -82,11 +87,13 @@ def setup_current_time():
     current_time = [hour, minute, second]
     return current_time
 
+
 def is_new_hour(current_time):
     """
     Check whether or not it's currently a new hour, return True if it is otherwise False
     """
     return True if current_time[1] == "00" and current_time[2] == "00" else False
+
 
 def tweet(twitter_api, ani_bm, bitly_api):
     """
@@ -105,11 +112,13 @@ def tweet(twitter_api, ani_bm, bitly_api):
             image_url = post.url
 
             while True:
-                try: # Try to post
-                    twitter_api.update_with_media(file = BytesIO(requests.get(image_url).content), filename = image_url.split('/')[-1].split('#')[0].split('?')[0], status = f"אני_במציאות {post_url}")
+                try:  # Try to post
+                    twitter_api.update_with_media(file=BytesIO(requests.get(image_url).content),
+                                                  filename=image_url.split('/')[-1].split('#')[0].split('?')[0],
+                                                  status=f"אני_במציאות {post_url}")
                     print("Posted")
                     break
-                except: # Choose a different post if it fails (probably becuase the chosen post does not contain an image)
+                except Exception:  # Choose a different post if it fails (probably becuase the chosen post does not contain an image)
                     print("Post does not contain an image, choosing a different post")
                     post = choose_post(setup_reddit())
                     post_url = shorten_link(post.permalink, bitly_api)
@@ -122,7 +131,9 @@ def tweet(twitter_api, ani_bm, bitly_api):
         # Sleep a second after every check
         time.sleep(1)
 
+
 def main():
     tweet(setup_twitter(), setup_reddit(), setup_bitly())
+
 
 main()
